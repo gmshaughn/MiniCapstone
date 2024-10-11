e7a56a709276438fca4e87df2951961378cd3343
gmshaughn <gregshaughn@gmail.com>
2024-10-11 16:09:35 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { GregsPage } from "./GregsPage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <GregsPage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
